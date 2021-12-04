16dded7ec524d64186072da62296f6d2e8b09635
Emem Isaac <me@ememisaac.com>
2021-12-04 17:32:15 +0100

    Uses the useNotes hook to source and pass data to the NotesList component

--- components/App.js
+++ components/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import useNotes from '../hooks/useNotes';
 import NoteList from './NoteList';
 
 function App() {
+  
+  const {notesData, notesDataError} = useNotes();
+
+  if(notesDataError){
+    return<div>error: {notesDataError}</div>;
+  }
+  if(!notesData){
+    return <div>...loading </div>;
+  }
+
   return (
     <div className="container">
       <h1><i class="fas fa-book-open"></i> My Note Book</h1>
-      <NoteList/>
+      <NoteList notesData={notesData}/>
     </div>
   );
 }
