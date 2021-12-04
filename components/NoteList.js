96dd7e9c7441baf83f0739a047732095bb824b00
Emem Isaac <me@ememisaac.com>
2021-12-04 17:30:39 +0100

    Sort notes by date

--- components/NoteList.js
+++ components/NoteList.js
@@ -2,10 +2,16 @@ import React from 'react';
 import NoteCard from './NoteCard';
 
 function NoteList({notesData}) {
+  
+  function sortByDate(a,b){
+    const dateA = a.createDate;
+    const dateB = b.createDate;
+    return dateA > dateB ? -1 : dateA < dateB ? 1 : 0;
+  }
 
   return (
     <div className="row tab-content bg-transparent note-has-grid">
-      {notesData.map(note => <NoteCard note={note} key={note.id} />)}
+      {notesData.sort(sortByDate).map(note => <NoteCard note={note} key={note.id} />)}
     </div>
   );
 }
