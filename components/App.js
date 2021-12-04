0945f73666fdf58978388e0dfab1fbcea023386c
Emem Isaac <me@ememisaac.com>
2021-12-04 15:52:59 +0100

    Makes app display the notes list

--- components/App.js
+++ components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import NoteList from './NoteList';
 
 function App() {
   return (
     <div className="container">
       <h1><i class="fas fa-book-open"></i> My Note Book</h1>
+      <NoteList/>
     </div>
   );
 }
