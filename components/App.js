e88784a57d0012ba1fb9327a5dae3c4d23856f38
Emem Isaac <me@ememisaac.com>
2021-12-04 15:24:36 +0100

    Adds the app component

---
+++ components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container">
+      <h1><i class="fas fa-book-open"></i> My Note Book</h1>
+    </div>
+  );
+}
+
+export default App;
